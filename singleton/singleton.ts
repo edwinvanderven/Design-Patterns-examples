40572f41f0a655963f98a04a2b608746eea8220d
Edwin van der Ven <edwinvanderven@gmail.com>
2020-01-05 12:11:58 +0100

    Added a code example of the singleton pattern

---
+++ singleton/singleton.ts
@@ -0,0 +1,24 @@
+class Singleton {
+  private static instance: Singleton;
+
+  private constructor() {
+    // the constructor being private means it is protected from external initialization
+  }
+
+  public static getInstance(): Singleton {
+      if (this.instance === undefined) {
+        this.instance = new Singleton();
+      }
+
+      return this.instance;
+  }
+}
+
+class SingletonExample {
+  constructor() {
+    const firstInstance = Singleton.getInstance();
+    const secondInstance = Singleton.getInstance();
+
+    console.log('The singleton class works: ', firstInstance === secondInstance);
+  }
+}
