d54d521ca5d2d364c8f3f23358f38afd3d1afe19
Edwin van der Ven <edwinvanderven@gmail.com>
2020-01-15 20:31:08 +0100

    Added a code example for the command pattern

---
+++ command/index.ts
@@ -0,0 +1,50 @@
+interface Command {
+  execute(): void;
+  undo(): void;
+  destroy(): void;
+}
+
+class SimpleCommand implements Command {
+  private operation: string;
+
+  constructor(operation: string) {
+    this.operation = operation;
+  }
+
+  public execute(): void {
+    console.log('Execute the SimpleCommand: ', this.operation);
+  }
+
+  public undo(): void {
+    console.log('Undo the SimpleCommand: ', this.operation);
+  }
+
+  public destroy(): void {
+    this.operation = undefined;
+  }
+}
+
+class Invoker {
+  private commands: Command[] = [];
+
+  public addCommand(command: Command) {
+    this.commands.push(command);
+  }
+
+  public clearCommands() {
+    this.commands.forEach((command: Command) => {
+      command.destroy();
+    });
+    this.commands = [];
+  }
+
+  public runCommands(): void {
+    this.commands.forEach((command: Command) => {
+      command.execute();
+    });
+  }
+}
+
+const invoker = new Invoker();
+invoker.addCommand(new SimpleCommand('Say Hi!'));
+invoker.runCommands();
