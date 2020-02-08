669e2b1df11d5c0e1bbe5b6f79b2aeb1c55b3e5e
Edwin van der Ven <edwinvanderven@gmail.com>
2020-02-08 13:00:34 +0100

    Added a code example for the bridge pattern

---
+++ Structural/bridge/index.ts
@@ -0,0 +1,43 @@
+class Abstraction {
+  protected implementation: Implementation;
+
+  constructor(implementation: Implementation) {
+      this.implementation = implementation;
+  }
+
+  public operation(): string {
+      const result = this.implementation.operationImplementation();
+      return 'Abstraction: Base operation with: ' + result;
+  }
+}
+
+class ExtendedAbstraction extends Abstraction {
+  public operation(): string {
+      const result = this.implementation.operationImplementation();
+      return 'ExtendedAbstraction: Extended operation with: ' + result;
+  }
+}
+
+interface Implementation {
+  operationImplementation(): string;
+}
+
+class ImplementationA implements Implementation {
+  public operationImplementation(): string {
+      return 'ImplementationA';
+  }
+}
+
+class ImplementationB implements Implementation {
+  public operationImplementation(): string {
+      return 'ImplementationB';
+  }
+}
+
+let implementation = new ImplementationA();
+let abstraction = new Abstraction(implementation);
+console.log(abstraction.operation());
+
+implementation = new ImplementationB();
+abstraction = new ExtendedAbstraction(implementation);
+console.log(abstraction.operation());
