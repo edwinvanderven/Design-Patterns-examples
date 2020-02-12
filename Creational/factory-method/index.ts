6b3c0af0977a2a1dd5422a8fedd06472ace4d00e
Edwin van der Ven <edwinvanderven@gmail.com>
2020-02-12 20:37:19 +0100

    Added a code example for the Factory method pattern

---
+++ Creational/factory-method/index.ts
@@ -0,0 +1,43 @@
+interface Product {
+  operation(): string;
+}
+
+interface CreatorInterface {
+  factoryMethod(): Product;
+}
+
+abstract class Creator implements CreatorInterface {
+  public abstract factoryMethod(): Product;
+
+  public someOperation(): string {
+      const product = this.factoryMethod();
+      return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  }
+}
+
+class ConcreteCreator1 extends Creator {
+  public factoryMethod(): Product {
+      return new ConcreteProduct1();
+  }
+}
+
+class ConcreteCreator2 extends Creator {
+  public factoryMethod(): Product {
+      return new ConcreteProduct2();
+  }
+}
+
+class ConcreteProduct1 implements Product {
+  public operation(): string {
+      return '{Result of the ConcreteProduct1}';
+  }
+}
+
+class ConcreteProduct2 implements Product {
+  public operation(): string {
+      return '{Result of the ConcreteProduct2}';
+  }
+}
+
+console.log(new ConcreteCreator1().someOperation());
+console.log(new ConcreteCreator2().someOperation());
